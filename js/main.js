44ce65a31816cd98c1c8b39a682b52d9e9153cc5
edward <erlkonig10@gmail.com>
2025-12-26 16:34:54 -0500

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,27 @@
+const canvas = document.getElementById("gameCanvas");
+canvas.width = 800;
+canvas.height = 600;
+const ctx = canvas.getContext("2d");
+
+const keys = {};
+window.addEventListener('keydown', e => keys[e.key] = true);
+window.addEventListener('keyup', e => keys[e.key] = false);
+
+const game = new Game(ctx);
+game.init(canvas);
+
+let lastTime = performance.now();
+
+function loop(timestamp){
+    const dt = (timestamp - lastTime)/1000; // delta time in seconds
+    lastTime = timestamp;
+
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+
+    game.update(dt);
+    game.draw();
+
+    requestAnimationFrame(loop);
+}
+
+requestAnimationFrame(loop);
