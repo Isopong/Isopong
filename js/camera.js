bd351443e19083fd8b899d608d0d07ebb47bec21
edward <erlkonig10@gmail.com>
2025-12-26 19:03:14 -0500

    Update camera.js

--- js/camera.js
+++ js/camera.js
@@ -1,22 +1,25 @@
 class Camera {
     constructor(canvas) {
-        this.pos = { x: 0, y: 0 };
-        this.zoom = 1;
         this.canvas = canvas;
-        this.easing = 0.05;
+        this.x = 0;
+        this.y = 0;
+        this.zoom = 1;
         this.target = null;
     }
 
     update() {
         if (!this.target) return;
-        this.pos.x += (this.target.pos.x - this.pos.x) * this.easing;
-        this.pos.y += (this.target.pos.y - this.pos.y) * this.easing;
 
-        const targetZoom = 1 - (this.target.z / this.target.maxZ) * 0.1;
-        this.zoom += (targetZoom - this.zoom) * this.easing;
+        this.x += (this.target.pos.x - this.canvas.width / 2 - this.x) * 0.05;
+        this.y += (this.target.pos.y - this.canvas.height / 2 - this.y) * 0.05;
+
+        const targetZoom = 1 + this.target.z * 0.01;
+        this.zoom += (targetZoom - this.zoom) * 0.05;
     }
 
     apply(ctx) {
-        ctx.setTransform(this.zoom, 0, 0, this.zoom, this.canvas.width / 2 - this.pos.x * this.zoom, this.canvas.height / 2 - this.pos.y * this.zoom);
+        ctx.translate(this.canvas.width / 2, this.canvas.height / 2);
+        ctx.scale(this.zoom, this.zoom);
+        ctx.translate(-this.canvas.width / 2 + this.x, -this.canvas.height / 2 + this.y);
     }
 }
