cfcd104331a0fbf787548e47ebdf523be297e945
edward (creator of isopong game) <erlkonig10@gmail.com>
2026-01-01 15:26:13 -0500

    Update camera.js

--- js/camera.js
+++ js/camera.js
@@ -1,22 +1,22 @@
 class Camera {
-    constructor(canvas) {
-        this.canvas = canvas;
+    constructor() {
         this.x = 0;
         this.y = 0;
-        this.target = null;
+        this.scale = 1;
     }
 
-    update() {
-        if (!this.target) return;
-
-        // tiny horizontal pan only, doesn't fly out of control like last time yay
-        const maxPan = 24;
-        const desiredX = (this.target.pos.x - this.canvas.width / 2) * 0.05;
-
-        this.x += (Math.max(-maxPan, Math.min(maxPan, desiredX)) - this.x) * 0.08;
+    reset(ctx) {
+        ctx.setTransform(1, 0, 0, 1, 0, 0);
     }
 
     apply(ctx) {
-        ctx.translate(-Math.round(this.x), 0);
+        ctx.setTransform(
+            this.scale,
+            0,
+            0,
+            this.scale,
+            this.x,
+            this.y
+        );
     }
 }
