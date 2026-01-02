3e47bbb1bc724dd308f57cc31be21b1aa411b4c6
edward (creator of isopong game) <erlkonig10@gmail.com>
2026-01-02 10:09:04 -0500

    Update game.js

--- js/game.js
+++ js/game.js
@@ -1,50 +1,49 @@
 class Game {
-    constructor(canvas) {
-        this.ctx = canvas.getContext("2d");
-        this.camera = new Camera();
+  constructor(canvas) {
+    this.ctx = canvas.getContext("2d");
 
-        // Table sprite
-        this.table = new Image();
-        this.table.src = "assets/sprites/table.png";
+    this.table = new Image();
+    this.table.src = "assets/sprites/table.png";
 
-        // Player paddle controlled by mouse
-        this.paddle = new Paddle();
+    this.paddle = new Paddle(canvas);
+    this.ball = new Ball(this.paddle);
 
-        // Ball
-        this.ball = new Ball(this.paddle);
+    this.lastTime = 0;
 
-        this.lastTime = 0;
-    }
+    window.addEventListener("keydown", (e) => {
+      if (e.key.toLowerCase() === "s") {
+        this.ball.serve();
+      }
+    });
+  }
 
-    start() {
-        requestAnimationFrame(this.loop.bind(this));
-    }
+  start() {
+    requestAnimationFrame(this.loop.bind(this));
+  }
 
-    loop(timestamp) {
-        const dt = (timestamp - this.lastTime) / 1000 || 0;
-        this.lastTime = timestamp;
+  loop(time) {
+    const dt = (time - this.lastTime) / 1000 || 0;
+    this.lastTime = time;
 
-        this.update(dt);
-        this.draw();
+    this.update(dt);
+    this.draw();
 
-        requestAnimationFrame(this.loop.bind(this));
-    }
+    requestAnimationFrame(this.loop.bind(this));
+  }
 
-    update(dt) {
-        this.paddle.update(dt);
-        this.ball.update(dt, this.paddle);
-    }
+  update(dt) {
+    this.paddle.update(dt);
+    this.ball.update(dt, this.paddle);
+  }
 
-    draw() {
-        const ctx = this.ctx;
-        ctx.imageSmoothingEnabled = false;
-        ctx.clearRect(0, 0, 960, 540);
+  draw() {
+    const ctx = this.ctx;
+    ctx.imageSmoothingEnabled = false;
+    ctx.clearRect(0, 0, 960, 540);
 
-        // Draw table
-        ctx.drawImage(this.table, 160, 80, 640, 380);
+    ctx.drawImage(this.table, 160, 80, 640, 380);
 
-        // Draw ball and paddle
-        this.ball.draw(ctx);
-        this.paddle.draw(ctx);
-    }
+    this.ball.draw(ctx);
+    this.paddle.draw(ctx);
+  }
 }
