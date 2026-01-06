4e34a945959c644d82b763cac8191addb16112c8
edward (creator of isopong game) <erlkonig10@gmail.com>
2026-01-06 10:49:51 -0500

    Update paddle.js

--- js/paddle.js
+++ js/paddle.js
@@ -1,49 +1,23 @@
 class Paddle {
-  constructor(x, y) {
-    this.x = x;
-    this.y = y;
+    constructor(x, y) {
+        this.pos = { x, y };
+        this.width = 16;
+        this.height = 80;
+    }
 
-    this.vx = 0;
-    this.vy = 0;
+    update(mouse) {
+        // Direct tracking (NO smoothing yet)
+        this.pos.y = mouse.y;
+        this.pos.x = mouse.x;
+    }
 
-    this.prevX = x;
-    this.prevY = y;
-
-    this.width = 14;
-    this.height = 70;
-
-    // MUCH tighter response
-    this.stiffness = 260;
-    this.damping = 0.94;
-
-    this.sprite = new Image();
-    this.sprite.src = "assets/sprites/paddle.png";
-  }
-
-  update(cursorX, cursorY, dt) {
-    this.prevX = this.x;
-    this.prevY = this.y;
-
-    const fx = (cursorX - this.x) * this.stiffness;
-    const fy = (cursorY - this.y) * this.stiffness;
-
-    this.vx += fx * dt;
-    this.vy += fy * dt;
-
-    this.vx *= this.damping;
-    this.vy *= this.damping;
-
-    this.x += this.vx * dt;
-    this.y += this.vy * dt;
-  }
-
-  draw(ctx) {
-    ctx.drawImage(
-      this.sprite,
-      this.x - this.width / 2,
-      this.y - this.height / 2,
-      this.width,
-      this.height
-    );
-  }
+    draw(ctx) {
+        ctx.fillStyle = "#b03030";
+        ctx.fillRect(
+            this.pos.x - this.width / 2,
+            this.pos.y - this.height / 2,
+            this.width,
+            this.height
+        );
+    }
 }
